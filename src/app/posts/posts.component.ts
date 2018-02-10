0ee38501c8acc805fa4b27bd7e0b7e8cc4666726
Ali <ali@alighali.com>
2018-02-10 18:57:21 +0100

    update post componenet

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
+
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  postsObservable : Observable <any[]>;
+
+  constructor( private db : AngularFireDatabase ) { }
 
   ngOnInit() {
+    this.postsObservable = this.getPosts('/posts');
   }
+
+  getPosts( listPath ): Observable <any[]> {
+    return this.db.list( listPath ).valueChanges();
+  }
+  
 
 }

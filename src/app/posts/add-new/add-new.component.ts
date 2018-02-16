1a219de6412ac3f921ad2b770d93d367edd8433a
Ali <ali@alighali.com>
2018-02-16 23:47:02 +0100

    update data list from firebase

--- src/app/posts/add-new/add-new.component.ts
+++ src/app/posts/add-new/add-new.component.ts
@@ -4,34 +4,46 @@ import { Observable } from 'rxjs/Observable';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms'
 import { NgForm } from '@angular/forms/src/directives/ng_form';
+import * as $ from 'jquery';
+import { Data } from '@angular/router/src/config';
+
+declare var jquery:any;
+declare var javascript:any
 
 @Component({
   selector: 'app-add-new',
   templateUrl: './add-new.component.html',
   styleUrls: ['./add-new.component.css']
 })
+
 export class AddNewComponent implements OnInit {
+  
   posts: Observable<any>;
   firstName: any ;
+  postTime: Data;
   constructor( private db: AngularFireDatabase ) { 
-
   }
    
-  ngOnInit() { 
+  ngOnInit() {
+    this.postTime= new Date();
     this.posts = this.db.object( '/posts' ).valueChanges();
   } 
 
   addNewPost(f: NgForm ) {
     console.log(f.value);
-    this.db.list('posts').push(f.value)
+    this.db.list('posts').push(f.value);
+    $('')
+    $('#addPost--form').css('display', 'none');
+    console.log(document.getElementById('newInput').innerHTML)
   }
 
-  getPosts( listPath ): Observable <any[]> {
-    return this.db.list( listPath ).valueChanges();
+  
+
+  deletePost(post:any) {
+
   }
 
-  showFirstName($event) {
-    console.log($event.target.value);
+  showFirstName() {
     console.log(this.firstName)
   }
 
--- src/app/posts/posts-home/posts-home.component.spec.ts
+++ src/app/posts/posts-home/posts-home.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { PostsHomeComponent } from './posts-home.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+  let component: PostsHomeComponent;
+  let fixture: ComponentFixture<PostsHomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ PostsHomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(PostsHomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
